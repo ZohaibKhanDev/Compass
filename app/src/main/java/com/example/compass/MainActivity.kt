3af399dafc049525e55267f32f793cf03c63f467
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-08-30 13:23:07 +0500

    Bug Fix

--- app/src/main/java/com/example/compass/MainActivity.kt
+++ app/src/main/java/com/example/compass/MainActivity.kt
@@ -248,10 +248,8 @@ fun CompassCanvas(primaryAngle: Float) {
     }
 }
 
-
 fun isAngleBetween(angle1: Float, angle2: Float, angleTolerance: Float): Boolean {
     val diff = Math.abs(angle1 - angle2)
     return diff < angleTolerance || diff > 360 - angleTolerance
 }
-
 
